89588be9a3ad85ec8d234237c21dc63cb80b3f98
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-07 01:50:21 +0300

    Mapped all names to NamesView

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
       <router-link to="/">Names</router-link> |
       <router-link to="/random-jokes">Random Jokes</router-link>
     </nav>
-    <router-view />
+    <div class="container">
+      <router-view />
+    </div>
   </div>
 </template>
 
--- src/views/NamesView.vue
+++ src/views/NamesView.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="names-view">
     <h1>Names</h1>
+    <ul>
+      <li v-for="{ id, name, surname } in namePicker.names" :key="id">
+        {{ name }} {{ surname }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 import { defineComponent } from "vue";
+import { mapState } from "vuex";
 
 export default defineComponent({
   name: "NamesView",
+  computed: {
+    ...mapState(["namePicker"]),
+  },
 });
 </script>
