6cc5232b04dbe11d2d4004abd069d69900e9dac0
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-08 00:03:35 +0300

    Defined RootState

--- src/shims-vuex.d.ts
+++ src/shims-vuex.d.ts
@@ -1,7 +1,15 @@
 import { Store } from "@/store"; // path to store file
+import { JokesData } from "@/models/JokesModel";
+import { Names } from "@/models/NamesModel";
 
 declare module "@vue/runtime-core" {
+  // declare your own store states
+  interface RootState {
+    data: JokesData;
+    names: Names[];
+  }
+  // provide typings for `this.$store`
   interface ComponentCustomProperties {
-    $store: Store;
+    $store: Store<RootState>;
   }
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,15 @@
 import { createStore } from "vuex";
-import namePicker from "@/store/modules/namePicker";
+import names, { NamesState } from "@/store/modules/names";
+import jokes, { JokesState } from "@/store/modules/jokes";
+
+export interface RootState {
+  names: NamesState;
+  data: JokesState;
+}
 
 export default createStore({
   modules: {
-    namePicker,
+    names,
+    jokes,
   },
 });
