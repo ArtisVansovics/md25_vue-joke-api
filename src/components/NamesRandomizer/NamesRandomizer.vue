2ef86b386846fd9089753340bcfa3dbd6913a8e5
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-08 18:54:43 +0300

    If notSelected array has names in it, a new random name is shown, when the "Select new random name" button is pressed. If there are no names left, all names are displayed and a random name is selected by pressing the "Select random name" button.

--- src/components/NamesRandomizer/NamesRandomizer.vue
+++ src/components/NamesRandomizer/NamesRandomizer.vue
@@ -1,21 +1,31 @@
 <template>
   <div class="container">
-    <div class="names-box">
+    <div v-if="notSelected.length > 0" class="names-box">
       <p v-for="{ id, name, surname } in notSelected" :key="id">
         {{ name }} {{ surname }}
       </p>
     </div>
-    <div class="result-box">
-      <p></p>
+    <div v-else class="names-box">
+      <p v-for="{ id, name, surname } in names" :key="id">
+        {{ name }} {{ surname }}
+      </p>
     </div>
-    <ButtonPrimary>Select random name</ButtonPrimary>
+    <div class="result-box">
+      <p>{{ selectedName.name }} {{ selectedName.surname }}</p>
+    </div>
+    <ButtonPrimary v-if="notSelected.length > 0" :onClick="selectNewName"
+      >Select new random name</ButtonPrimary
+    >
+    <ButtonPrimary v-else :onClick="selectRandomName"
+      >Select random name</ButtonPrimary
+    >
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import ButtonPrimary from "@/components/ButtonPrimary/ButtonPrimary.vue";
-import { mapGetters, mapState } from "vuex";
+import { mapActions, mapGetters, mapState } from "vuex";
 
 export default defineComponent({
   name: "NamesRandomizer",
@@ -25,8 +35,14 @@ export default defineComponent({
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
       names: (state) => state.names,
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      selectedName: (state) => state.selectedName,
     }),
     ...mapGetters("names", ["notSelected"]),
+  },
+  methods: {
+    ...mapActions("names", ["selectNewName", "selectRandomName"]),
   },
 });
 </script>
--- src/store/modules/names.ts
+++ src/store/modules/names.ts
@@ -4,124 +4,13 @@ import { RootState } from "@/store";
 
 export interface NamesState {
   names: Name[];
+  selectedName: Name;
 }
 
 type Context = ActionContext<NamesState, RootState>;
 
 export default {
   namespaced: true,
-  // state: (): NamesState => ({
-  //   names: [
-  //     {
-  //       id: 1,
-  //       name: "Alberts",
-  //       surname: "Ivāns",
-  //       picked: false,
-  //     },
-  //     {
-  //       id: 2,
-  //       name: "Arnis",
-  //       surname: "Toks",
-  //       picked: false,
-  //     },
-  //     {
-  //       id: 3,
-  //       name: "Artis",
-  //       surname: "Vansovičs",
-  //       picked: false,
-  //     },
-  //     {
-  //       id: 4,
-  //       name: "Artūrs",
-  //       surname: "Dancis",
-  //       picked: false,
-  //     },
-  //     {
-  //       id: 5,
-  //       name: "Bogdans",
-  //       surname: "Zabela",
-  //       picked: false,
-  //     },
-  //     {
-  //       id: 6,
-  //       name: "Edgars",
-  //       surname: "Kapilinskis",
-  //       picked: false,
-  //     },
-  //     {
-  //       id: 7,
-  //       name: "Elvis",
-  //       surname: "Ketners",
-  //       picked: false,
-  //     },
-  //     {
-  //       id: 8,
-  //       name: "Harals",
-  //       surname: "",
-  //       picked: false,
-  //     },
-  //     {
-  //       id: 9,
-  //       name: "Ieva",
-  //       surname: "Dīvāne",
-  //       picked: false,
-  //     },
-  //     {
-  //       id: 10,
-  //       name: "Ilona",
-  //       surname: "",
-  //       picked: false,
-  //     },
-  //     {
-  //       id: 12,
-  //       name: "Inta",
-  //       surname: "Ozoliņa",
-  //       picked: false,
-  //     },
-  //     {
-  //       id: 13,
-  //       name: "Jānis",
-  //       surname: "Šijānovs",
-  //       picked: false,
-  //     },
-  //     {
-  //       id: 14,
-  //       name: "Ņikita",
-  //       surname: "Peškovs",
-  //       picked: false,
-  //     },
-  //     {
-  //       id: 15,
-  //       name: "Niklāvs",
-  //       surname: "Romans",
-  //       picked: false,
-  //     },
-  //     {
-  //       id: 16,
-  //       name: "Regīna",
-  //       surname: "",
-  //       picked: false,
-  //     },
-  //     {
-  //       id: 17,
-  //       name: "Ričards",
-  //       surname: "Dzelme",
-  //       picked: false,
-  //     },
-  //     {
-  //       id: 18,
-  //       name: "Rihards",
-  //       surname: "Karlauskis",
-  //       picked: false,
-  //     },
-  //     {
-  //       id: 19,
-  //       name: "Roberts",
-  //       surname: "Lode",
-  //       picked: false,
-  //     },
-  //   ] as Name[],
-  // }),
   state: {
     names: [
       {
@@ -233,12 +122,37 @@ export default {
         picked: false,
       },
     ] as Name[],
+    selectedName: {} as Name,
   },
   getters: {
     notSelected(state: NamesState) {
       return state.names.filter((name) => !name.picked);
     },
   },
-  actions: {},
-  mutations: {},
+  actions: {
+    selectNewName(context: Context) {
+      context.commit("selectNewName");
+    },
+    selectRandomName(context: Context) {
+      context.commit("selectRandomName");
+    },
+  },
+  mutations: {
+    selectNewName(state: NamesState) {
+      const unselectedNames = state.names.filter((name) => !name.picked);
+      const randomName =
+        unselectedNames[Math.floor(Math.random() * unselectedNames.length)];
+      randomName.picked = true;
+      state.selectedName = randomName;
+    },
+    selectRandomName(state: NamesState) {
+      const notPreviousNames = state.names.filter(
+        (name) => name.id !== state.selectedName.id
+      );
+      const randomName =
+        notPreviousNames[Math.floor(Math.random() * notPreviousNames.length)];
+      randomName.picked = true;
+      state.selectedName = randomName;
+    },
+  },
 };
