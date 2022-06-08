73699d2b804700ba5f25783d677a6e31b39891d0
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-08 12:42:48 +0300

    Fixed jokes api request by adding store dispatch to the mounted hook

--- src/views/JokesView.vue
+++ src/views/JokesView.vue
@@ -1,25 +1,26 @@
 <template>
   <div class="names-view">
     <h1>Random Jokes</h1>
+    <p v-for="{ joke, id } in jokes.data.jokes" :key="id">{{ joke }}</p>
     {{ test }}
   </div>
 </template>
 
 <script>
 import { defineComponent } from "vue";
-import { mapActions, mapGetters, mapState } from "vuex";
+import { mapGetters, mapState } from "vuex";
 
 export default defineComponent({
   name: "JokesView",
+  mounted() {
+    this.$store.dispatch("jokes/getData");
+  },
   computed: {
     ...mapState(["jokes"]),
     ...mapGetters(["jokes/data"]),
     test() {
       return console.log(this.jokes);
     },
-  },
-  methods: {
-    ...mapActions(["jokes/getData"]),
   },
 });
 </script>
