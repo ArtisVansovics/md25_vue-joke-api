aec4d6c3bc36e064677a7e4ea3fe90eab18aad71
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-08 19:37:01 +0300

    Created JokesCard to display a joke

---
+++ src/components/JokesCard/JokesCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="card">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "JokesCard",
+});
+</script>
+
+<style scoped lang="scss" src="./JokesCard.scss" />
