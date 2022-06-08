cfcb8d5450fbf7ed0bec068feeb277dd8b9d5c7d
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-09 00:38:26 +0300

    Created path to specific joke based on its id

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import NamesView from "@/views/NamesView.vue";
 import JokesView from "@/views/JokesView.vue";
+import JokeView from "@/views/JokeView/JokeView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -14,6 +15,11 @@ const routes: Array<RouteRecordRaw> = [
     component: JokesView,
     // component: () =>
     //   import(/* webpackChunkName: "about" */ "../views/JokesView.vue"),
+  },
+  {
+    path: "/random-jokes/:id",
+    name: "joke",
+    component: JokeView,
   },
 ];
 
